b5fe8a091c666366fa57628f11f38b4d6e18b90d
The Murus Team <info@murus.it>
2022-12-09 15:20:17 +0100

    fix

--- FreeScaler/AppDelegate.swift
+++ FreeScaler/AppDelegate.swift
@@ -29,8 +29,6 @@ import Cocoa
 // info@murus.it
 
 
-
-
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) { self.startFreeScaler() }
